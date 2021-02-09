0b80b63d26f40e6be699e1ad63e916d6dd77db10
rukanaction <ruka.34ever57@gmail.com>
2021-02-09 14:54:53 +0900

    Initial Commit

---
+++ Gacha2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gacha2
+//
+//  Created by Ruka Saito on 2021/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
